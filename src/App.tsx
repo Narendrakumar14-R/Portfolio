2424b70b9293a48626691def526902af870bfeb8
narendrakumar14-R <narendrarakumarr2023@gmail.com>
2025-09-09 17:37:22 -0500

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* 👇 Add basename="/Portfolio" here */}
+      <BrowserRouter basename="/Portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
